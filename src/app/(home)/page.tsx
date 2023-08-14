cc8c83799a99ac4c194af9e359483ad8857ab946
igorviniciussantana <contact.igorvinicius@gmail.com>
2023-08-14 15:49:09 -0400

    feat: create Menu component

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,10 @@
+import Menu from '@/components/Menu/Menu'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+     <Menu/>
+    </main>
+  )
+}
---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Menu(){
+return (
+
+<div className="menu-wrapper">
+    <h1>Selene Tecnologia</h1>
+</div>
+
+
+)
+
+}
