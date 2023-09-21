6eb17da6d09d76bcef4d2d30aadb5f6e873233b7
igorviniciussantana <contact.igorvinicius@gmail.com>
2023-09-21 16:50:03 -0400

    feat: finish footer and some other adjustments

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,7 +6,9 @@ import { MenuItems } from "./../Menu/items";
 export default function Footer() {
   return (
     <footer className={styles.footer_wrapper}>
+
       <div className={styles.footer_content}>
+
         <div className={styles.footer_left}>
           <img src="logo-light.svg" className={styles.footer_logo} />
           <p>
@@ -14,21 +16,38 @@ export default function Footer() {
             inovadoras
           </p>
         </div>
+
         <div className={styles.footer_right}>
-          <h2>Acesse</h2>
-          <ul>
-            {MenuItems.map((item) => {
-              return (
-                <li>
-                  <img src="/next.svg" className={styles.li_icon}/>
-                  <Link href={item.url} key={item.url}>
-                    {item.name}
-                  </Link>
+          <div>
+            <h2>Acesse</h2>
+            <ul>
+              {MenuItems.map((item) => {
+                return (
+                  <li>
+                    <img src="/next.svg" className={styles.li_icon} />
+                    <Link href={item.url} key={item.url}>
+                      {item.name}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <div>
+            <h2>Contato</h2>
+            <ul>
+              <li>
+              <img src="/email.svg" className={styles.li_icon} />
+                <p className={styles.contact}>selene@gmail.com</p>
                 </li>
-              );
-            })}
-          </ul>
+                <li>
+              <img src="/whatsapp.svg" className={styles.li_icon} />
+                <p className={styles.contact}>+55 51 8187-7186</p>
+                </li>
+            </ul>
+          </div>
         </div>
+
       </div>
       <p className={styles.footer_copyright}>
         © SELENE TECNOLOGIA, 2023. TODOS OS DIREITOS RESERVADOS.
