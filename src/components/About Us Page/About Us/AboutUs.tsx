9c454997ce6c056659e1a89ecad219c2591ab4d1
igorviniciussantana <contact.igorvinicius@gmail.com>
2023-09-29 10:08:55 -0400

    feat: formatted about us texts

--- src/components/About Us Page/About Us/AboutUs.tsx
+++ src/components/About Us Page/About Us/AboutUs.tsx
@@ -34,7 +34,7 @@ export default function AboutUs() {
             Somos uma equipe apaixonada por tecnologia e especializada em
             desenvolvimento de software. Nosso objetivo é fornecer soluções
             inovadoras e personalizadas que atendam às necessidades exclusivas
-            dos nossos clientes. Combinamos expertise técnica, criatividade e
+            dos nossos clientes. <br/><br/>Combinamos expertise técnica, criatividade e
             comprometimento para entregar resultados excepcionais e impulsionar
             o sucesso dos negócios.
           </p>
@@ -63,7 +63,7 @@ export default function AboutUs() {
             qualidade que impulsionem o sucesso dos nossos clientes. Através do
             uso de tecnologias avançadas e de uma abordagem colaborativa,
             buscamos simplificar processos, aumentar a eficiência e agregar
-            valor aos negócios. Nosso compromisso é oferecer resultados
+            valor aos negócios. <br/><br/>Nosso compromisso é oferecer resultados
             excepcionais, aliados a um excelente atendimento ao cliente e à
             ética profissional, sempre com foco na satisfação e no crescimento
             dos nossos parceiros de negócios.
@@ -91,7 +91,7 @@ export default function AboutUs() {
             em diversas indústrias, desde e-commerce e fintech até saúde e
             educação. Trabalhamos com empresas de todos os tamanhos, desde
             startups promissoras até grandes corporações, adaptando nossos
-            serviços às necessidades específicas de cada cliente. Nossa equipe é
+            serviços às necessidades específicas de cada cliente. <br/><br/>Nossa equipe é
             composta por profissionais altamente qualificados e comprometidos em
             fornecer soluções de software de alta qualidade.
           </p>
@@ -120,7 +120,7 @@ export default function AboutUs() {
             empresa. Valorizamos a transparência em nossos relacionamentos com
             clientes, parceiros e colaboradores. Buscamos constantemente a
             inovação, mantendo-nos atualizados com as últimas tendências e
-            tecnologias. Nosso compromisso com a excelência reflete-se em cada
+            tecnologias.<br/><br/> Nosso compromisso com a excelência reflete-se em cada
             projeto que realizamos, desde a análise inicial até a implementação
             final.
           </p>
@@ -147,7 +147,7 @@ export default function AboutUs() {
             atendam às necessidades exclusivas de cada cliente. Nossa abordagem
             é centrada no cliente, buscando entender seus desafios e metas para
             desenvolver soluções sob medida que agreguem valor real ao seu
-            negócio. Trabalhamos em estreita colaboração com nossos clientes,
+            negócio. <br/><br/>Trabalhamos em estreita colaboração com nossos clientes,
             garantindo uma comunicação clara e eficiente em todas as etapas do
             projeto.
           </p>
