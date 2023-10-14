3ebaff5a01ca55ed38344303e26eb720d840e07a
igorviniciussantana <contact.igorvinicius@gmail.com>
2023-10-14 13:03:47 -0400

    feat: create portfolio page

---
+++ src/app/portfolio/projects.ts
@@ -0,0 +1,26 @@
+export const Projects = [
+  {
+    name: "Nome do Projeto",
+    description:
+      "Breve descrição do projeto, incluindo seus principais objetivos e resultados alcançados. Destaque as soluções inovadoras ou tecnologias específicas utilizadas, se aplicável.",
+    img: "",
+  },
+  {
+    name: "Nome do Projeto",
+    description:
+      "Breve descrição do projeto, incluindo seus principais objetivos e resultados alcançados. Destaque as soluções inovadoras ou tecnologias específicas utilizadas, se aplicável.",
+    img: "",
+  },
+  {
+    name: "Nome do Projeto",
+    description:
+      "Breve descrição do projeto, incluindo seus principais objetivos e resultados alcançados. Destaque as soluções inovadoras ou tecnologias específicas utilizadas, se aplicável.",
+    img: "",
+  },
+  {
+    name: "Nome do Projeto",
+    description:
+      "Breve descrição do projeto, incluindo seus principais objetivos e resultados alcançados. Destaque as soluções inovadoras ou tecnologias específicas utilizadas, se aplicável.",
+    img: "",
+  },
+];
