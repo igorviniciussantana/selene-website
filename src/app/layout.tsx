4f4b47b262c708c4d75050e195ee6fbdf5e79598
igor vinicius <86114583+igorviniciussantana@users.noreply.github.com>
2024-03-05 17:37:15 -0400

    update Body GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,15 +23,7 @@ export default function RootLayout({
   }, []);
   return (
     <html lang="pt-br">
-<head>
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-N9QNK29V');</script>
-<!-- End Google Tag Manager -->
-</head>
+
       <body className={space_grtsk.className}>
 <!-- Google Tag Manager (noscript) -->
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N9QNK29V"
