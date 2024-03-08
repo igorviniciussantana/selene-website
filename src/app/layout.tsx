e739aed84c49d5643f00b9686de12bb7058aff99
igorviniciussantana <iguivinigamer@gmail.com>
2024-03-08 19:30:22 -0400

    feat: add Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Footer from "@/components/Footer/Footer";
 import AOS from 'aos';
 import { useEffect } from 'react'
 import 'aos/dist/aos.css';
+import Head from "next/head";
 
 const space_grtsk = Space_Grotesk({ subsets: ["latin"] });
 
@@ -23,12 +24,28 @@ export default function RootLayout({
   }, []);
   return (
     <html lang="pt-br">
-
+<Head>
+<script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N9QNK29V');
+            `,
+          }}
+        />
+</Head>
       <body className={space_grtsk.className}>
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N9QNK29V"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+      <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N9QNK29V"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            `,
+          }}
+        />
         <Menu />
         {children}
         <Footer />
